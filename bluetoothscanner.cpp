e16c9164b7655dfcc185166e39d8b45ff3b3cb5e
Konrad Traczyk <konrad.traczyk122@gmail.com>
2017-04-08 21:48:01 +0200

    Przygotowanie do rozpoczecia inquiry

--- bluetoothscanner.cpp
+++ bluetoothscanner.cpp
@@ -5,9 +5,13 @@
 
 
 CBluetoothScanner::CBluetoothScanner(QObject *parent) : QObject(parent),
-  m_localAdapterDevId(-1)
+  m_localAdapterDevId(-1),
+  m_localAdapterSocket(-1),
+  m_inquiryInfo(nullptr)
 {
-
+    m_timeoutTimer.setInterval(M_CONNECTION_TIMEOUT_MS);
+    m_timeoutTimer.setSingleShot(true);
+    connect(&m_timeoutTimer, SIGNAL(timeout()), this, SLOT(onScanTimeout()));
 }
 
 CBluetoothScanner* CBluetoothScanner::GetInstance()
@@ -18,14 +22,27 @@ CBluetoothScanner* CBluetoothScanner::GetInstance()
 
 void CBluetoothScanner::m_openLocalAdapter()
 {
+    LOG_DBG(" ");
     const char* localAdapterMAC = "01:23:45:67:89:AB";;
 
     m_localAdapterDevId = hci_devid(localAdapterMAC);
 
     m_localAdapterSocket = hci_open_dev(m_localAdapterDevId);
 
+    LOG_DBG("m_localAdapterDevId: %d, m_localAdapterSocket: %d", m_localAdapterDevId, m_localAdapterSocket);
     if(m_localAdapterDevId < 0 || m_localAdapterSocket < 0)
     {
-
+        LOG_CRITICAL("FAILED TO CREATE SOCKET CONNECTING WITH LOCAL BLUETOOTH ADAPTER!");
+        exit(1);
     }
+}
+
+void CBluetoothScanner::m_startScanning()
+{
+
+}
+
+void CBluetoothScanner::onScanTimeout()
+{
+    hci_inquiry(m_localAdapterDevId, 8, m_maxScannedDevicesNumber, NULL, &m_inquiryInfo, IREQ_CACHE_FLUSH);
 }
--- bluetoothscanner.h
+++ bluetoothscanner.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include <QList>
+#include <QTimer>
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
@@ -22,16 +23,24 @@ public:
 signals:
 
 public slots:
-
+    void onScanTimeout();
 
 private:
     explicit CBluetoothScanner(QObject *parent = 0);
 
     void m_openLocalAdapter();
 
+    QTimer m_timeoutTimer;
+
+    const int M_CONNECTION_TIMEOUT_MS = 10000;
+
     int m_localAdapterDevId;
 
     int m_localAdapterSocket;
+
+    inquiry_info* m_inquiryInfo;
+
+    const int m_maxScannedDevicesNumber = 10;
 };
 
 #endif // BLUETOOTHSCANNER_H
